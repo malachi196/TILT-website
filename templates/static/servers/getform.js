a955239e570fe37776e2f1538e54cb263f2c87c2
malachi196 <malachiaaronwilson@gmail.com>
2024-04-29 10:06:47 -0700

    server debugging part 3

--- templates/static/servers/getform.js
+++ templates/static/servers/getform.js
@@ -1,24 +1,36 @@
-var uname = document.getElementById("uname").value;
-var qu = document.getElementById("qu").value;
+console.stdlog = console.log.bind(console)
+console.logs = []
+console.log = function(){
+    console.logs.push(Array.from(arguments));
+    console.stdlog.apply(console, arguments);
+}
+data = []
+
+function query_input() {
+    let uname = document.getElementById("uname");
+    let qu = document.getElementById("qu");
+    data.push(uname)
+    data.push(qu)
+    }
+
 
 function query_data() {
-    if (uname.trim() === "" || qu.trim() === "") 
+    if (data[0].trim() === "" || data[1].trim() === "") 
     {
-        alert("form data is mandatory")
+        alert("form data is mandatory");
+        return false;
     }
     //document.write("Thanks for contacting us! We will try to respond as soon as possible." + "<br>")
     //document.write("Name: " + uname + "<br>")
     //document.write("question/commment: " + "<br>")
     //document.write(qu)
+    return true;
 }
 
-function append_data() {
+function append_data(quElement, unameElement) {
     document.getElementById("editable-p1").innerHTML = "Thanks for contacting us! We will try to respond as soon as possible." + "<br>";
-    document.getElementById("editable-p2").innerHTML = "Name: " + uname + "<br>";
+    document.getElementById("editable-p2").innerHTML = "Name: " + data[0] + "<br>";
     document.getElementById("editable-p3").innerHTML = "Question/comment: " + "<br>";
-    document.getElementById("editable-p4").innerHTML = qu;
+    document.getElementById("editable-p4").innerHTML = data[1];
 }
 
-function get_donation() {
-    
-}
--- templates/static/servers/getform.php
+++ templates/static/servers/getform.php
@@ -3,7 +3,8 @@
         <?php
         $userfile = fopen("users.csv", "w") or die("unable to access user data at the current moment");
         $name = $_POST["name"]
-        fwrite($userfile, $name)
+        $qu = $_POST["qu"]
+        fwrite($userfile, [$name, $qu])
         ?>
         <p>form accepted</p>
     </body>
