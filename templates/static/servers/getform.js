7dea2bbbf252feef32b3b297f769244f4b68fcf3
malachi196 <malachiaaronwilson@gmail.com>
2024-04-28 21:04:11 -0700

    fix donations + internal server

--- templates/static/servers/getform.js
+++ templates/static/servers/getform.js
@@ -1,8 +1,22 @@
+let uname;
+let qu;
+
 function query_data() {
-    var uname = document.getElementById("uname").value;
-    var qu = document.getElementById("qu").value;
-    document.write("Thanks for contacting us! We will try to respond as soon as possible." + "<br>")
-    document.write("Name: " + uname + "<br>")
-    document.write("question/commment: " + "<br>")
-    document.write(qu)
+    uname = document.getElementById("uname").value;
+    qu = document.getElementById("qu").value;
+    if (uname.trim() === "" || qu.trim() === "") 
+    {
+        alert("form data is mandatory")
+    }
+    //document.write("Thanks for contacting us! We will try to respond as soon as possible." + "<br>")
+    //document.write("Name: " + uname + "<br>")
+    //document.write("question/commment: " + "<br>")
+    //document.write(qu)
+}
+
+function append_data() {
+    document.getElementById("editable-p1").innerHTML = "Thanks for contacting us! We will try to respond as soon as possible." + "<br>";
+    document.getElementById("editable-p2").innerHTML = "Name: " + uname + "<br>";
+    document.getElementById("editable-p3").innerHTML = "Question/comment: " + "<br>";
+    document.getElementById("editable-p4").innerHTML = qu;
 }
